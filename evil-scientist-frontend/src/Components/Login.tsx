98e38b91045f0f633199a93c5ea19de0a9dc2c88
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-02-04 09:58:00 -0500

    login view mostly done

--- evil-scientist-frontend/src/Components/Login.tsx
+++ evil-scientist-frontend/src/Components/Login.tsx
@@ -1,11 +1,32 @@
+import { Button, Container, Form } from "react-bootstrap"
+
 export const Login:React.FC = () => {
 
 
 
     return(
-        <>
-            <h1>Login</h1>
-        </>
+        // Bootstrap gives us this Container element - it does some default padding/centering
+        <Container>
+            <h1>Evil Scientist Corp. Login</h1>
+
+            <div>
+                <Form.Control
+                    type="text"
+                    placeholder="username"
+                    name="username"
+                />
+            </div>
+
+            <div>
+                <Form.Control
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                />
+            </div>
+
+            <Button className="btn-success m-1">Login</Button>
+        </Container>
     )
 
 }
