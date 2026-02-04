5633653f7b0590c579b3a39a1f189dc0ef9c0638
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-02-04 09:45:59 -0500

    installed bootstrap, setup routing

--- evil-scientist-frontend/src/App.tsx
+++ evil-scientist-frontend/src/App.tsx
@@ -1,4 +1,8 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Login } from './Components/Login'
+import { Dashboard } from './Components/Dashboard'
 
 function App() {
  
@@ -6,7 +10,12 @@ function App() {
   return (
     <>
       
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
