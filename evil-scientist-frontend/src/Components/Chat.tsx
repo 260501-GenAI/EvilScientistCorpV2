5867c11193d16eec32e98f507caaf2a8fe86b51a
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-02-06 10:12:40 -0500

    Chatbot in primitive but hopefully working state

--- evil-scientist-frontend/src/Components/Chat.tsx
+++ evil-scientist-frontend/src/Components/Chat.tsx
@@ -1,7 +1,35 @@
+import axios from "axios"
+import { useState } from "react"
 import { Button, Card, Form } from "react-bootstrap"
 
 export const Chat:React.FC = () => {
 
+    //Set up some state to capture LLM input and output
+    const [input, setInput] = useState<string>("")
+    const [response, setResponse] = useState<string>("")
+
+    //Function that sends the chat and captures the response
+    const sendMessage = async () => {
+
+        const response = await axios.post("http://127.0.0.1:8000/chat/memory-chat", {input:input})
+
+        //print out the response just to see it
+        console.log(response)
+
+        //set the response in state, wipe the input state
+        setResponse(response.data.response)
+        /* response.data.response?? wut
+
+            response 1 is the actual HTTP response
+            data is the data within the HTTP response
+            response 2 is the field in data called "response"*/
+
+        setInput("")
+
+        //temporary, to delete
+        alert(response)
+
+    }
 
     return(
         <Card>
@@ -10,13 +38,15 @@ export const Chat:React.FC = () => {
             <Form.Control
             type="text"
             placeholder="Send a message to your virtual assistant..."
-
+            value={input} //We need this for onChange 
+            //Whenever this input CHANGES, save the value in state
+            onChange={(event) => setInput(event.target.value)}
             className="mb-2"
             />
 
-            <Button>Send Message</Button>
+            <Button onClick={sendMessage}>Send Message</Button>
 
-            
+
         </Card>
     )
 
