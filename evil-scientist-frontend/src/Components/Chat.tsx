7048368ce42dabf74d865407375a28d48b460934
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-02-06 09:38:25 -0500

    pre-logic view done for now

--- evil-scientist-frontend/src/Components/Chat.tsx
+++ evil-scientist-frontend/src/Components/Chat.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap"
+import { Button, Card, Form } from "react-bootstrap"
 
 export const Chat:React.FC = () => {
 
@@ -6,6 +6,15 @@ export const Chat:React.FC = () => {
     return(
         <Card>
             <h4>Evil Scientist Chat</h4>
+
+            <Form.Control
+            type="text"
+            placeholder="Send a message to your virtual assistant..."
+
+            className="mb-2"
+            />
+
+            <Button>Send Message</Button>
 
             
         </Card>
